04225ae4e5e979e8a61619e503f71d2676bb5c13
scott lozano <scottloz@protonmail.com>
2025-07-05 20:16:25 +0100

    To do added

--- zerofox/accounting.go
+++ zerofox/accounting.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -25,7 +26,8 @@ type Alerts struct {
 	Alerts []Alert `json:"alerts"`
 }
 
-func fetchAlerts() {
+// Fetch and format alerts urls
+func fetchAlerts() []Alert {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -42,7 +44,7 @@ func fetchAlerts() {
 	fmt.Println("Request URL:", req.URL.String())
 	if err != nil {
 		log.Printf("Failed to create request: %s", err)
-		return
+		return []Alert{}
 	}
 	req.Header.Add("Authorization", zfToken)
 	client := &http.Client{}
@@ -51,7 +53,7 @@ func fetchAlerts() {
 
 	if err != nil {
 		log.Printf("Request Failed: %s", err)
-		return
+		return []Alert{}
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -63,12 +65,46 @@ func fetchAlerts() {
 	err = json.Unmarshal(body, &alerts)
 	if err != nil {
 		log.Printf("Reading body failed: %s", err)
-		return
+		return []Alert{}
 	}
 
 	log.Printf("Length of alerts: %d", len(alerts.Alerts))
+	onlyAlerts := alerts.Alerts[:0] // Create a slice to hold only the alerts
+	for _, alert := range alerts.Alerts {
+		alert.Url = cleanUrl(alert.Url)
+		if alert.Url == "" {
+			continue
+		} else {
+			onlyAlerts = append(onlyAlerts, alert)
+		}
+	}
 
+	if len(onlyAlerts) > 0 {
+		fmt.Println("Found", len(onlyAlerts), "alerts")
+		return onlyAlerts
+	} else {
+		fmt.Println("No alerts found")
+		return []Alert{}
+	}
 }
+
+func cleanUrl(rawUrl string) string {
+	cleaned := strings.TrimPrefix(rawUrl, "hxxp://")
+
+	parsed, err := url.Parse("http://" + cleaned)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return ""
+	}
+
+	host := parsed.Host
+	fmt.Println("Final domain:", host)
+	return host
+}
+
+// TO DO:
+// For each alert url, check on hitlist if it is resolved status
+// if resolved, cancel takedown and close alert in zerofox
 
 func main() {
 	fetchAlerts()
